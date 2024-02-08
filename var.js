c34c0264ec2df53a4d82fd60d298c3a9481a6881
ikmalpra97 <193040038.ilham@mail.unpas.ac.id>
2024-02-08 22:51:02 +0700

    learning about variable and their version

---
+++ var.js
@@ -0,0 +1,11 @@
+
+//pemanggilan variabel secara global
+// var iceCream = 12;
+
+//pemanggilan variabel yang isi nya bisa di manipulasia setelah inisialisasi
+let iceCream = 5;
+
+//pemanggialn variabel yang tidak bisa dirubah
+// const iceCream = 5;
+
+console.log(iceCream);
