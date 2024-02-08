5f5d082d68f61a4c31c1306b75a5f73fa2ca38aa
ikmalpra97 <193040038.ilham@mail.unpas.ac.id>
2024-02-08 22:58:18 +0700

    learning type data

--- var.js
+++ var.js
@@ -1,11 +1,21 @@
 
+// Learning Variable
 //pemanggilan variabel secara global
 // var iceCream = 12;
 
 //pemanggilan variabel yang isi nya bisa di manipulasia setelah inisialisasi
-let iceCream = 5;
+// let iceCream = 5;
 
 //pemanggialn variabel yang tidak bisa dirubah
 // const iceCream = 5;
 
-console.log(iceCream);
+// console.log(iceCream);
+
+// Learning Type Data
+// const iceCream = 5; (tipe data ini adalah number)
+// const iceCream = "Chocolate"; (tipe data ini adalah string)
+// const iceCream = true; (tipe data ini adalah boolean)
+// const iceCream = []; (tipe data ini adalah Object(array))
+// const iceCream = undefined; (tipe data ini adalah undefined)
+
+// console.log(typeof iceCream);
